cc95379691b04639af80195d0536fdac7ca4f09a
ThuanPham777 <phamchithuan777@gmail.com>
2024-11-05 23:51:37 +0700

    adjust id

--- controllers/productController.js
+++ controllers/productController.js
@@ -13,7 +13,7 @@ const getAllProducts = async (req, res) => {
 // Lấy sản phẩm theo ID
 const getProductById = async (req, res) => {
   try {
-    const product = await Product.findById(req.params._id);
+    const product = await Product.findById(req.params.id);
     if (!product) {
       return res.status(404).json({ message: 'Product not found' });
     }
